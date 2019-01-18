899672c1b9d3d01510f116525f9df95ce34f7793
Duch003 <tomaszmankin003@gmail.com>
2019-01-18 20:27:46 +0100

    Initial commit

---
+++ PrimeNumberDetector.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberDetector
+{
+    public class PrimeNumberDetector
+    {
+        //Source: https://en.wikipedia.org/wiki/Primality_test
+        public static bool IsPrimeNumberV1(ulong input)
+        {
+            //Check for first two numbers
+            if (input <= 3)
+                return input > 1;
+
+            //Checking most predictable numbers
+            if (input % 2 == 0 || input % 3 == 0)
+                return false;
+
+            //Simple prime-checking formula
+            for (ulong i = 5; (i * i) <= input; i += 6)
+            {
+                if (input % i == 0 || input % (i + 2) == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+
+        //Returns factors of given number except 1 and itself
+        public static List<ulong> GetFactors(ulong input)
+        {
+            List<ulong> output = new List<ulong>();
+
+            //2 and 3 are skipped because method returns only factors which are not 1 and number itself
+            if (input == 2 || input == 3)
+                return output;
+
+            for (ulong i = 2; i < input; i++)
+            {
+                if(input % i == 0)
+                    output.Add(i);
+            }
+
+            return output;
+        }
+
+        public static List<ulong> RetrievePrimeNumbers(List<ulong> input)
+        {
+            List<ulong> output = new List<ulong>();
+
+            foreach (var number in input)
+                if(IsPrimeNumberV1(number))
+                    output.Add(number);
+
+            return output;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrimeNumberDetector
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                Console.Clear();
+                Introduce();
+                var input = Console.ReadLine();
+
+                if (!ulong.TryParse(input, out ulong number))
+                {
+                    InputNotNumber();
+                    continue;
+                }
+
+                IsPrimeNumber(PrimeNumberDetector.IsPrimeNumberV1(number));
+
+                var allFactors = PrimeNumberDetector.GetFactors(number);
+
+                CountFactors(allFactors);
+
+                ShowPrimeFactors(allFactors);
+
+                Console.ReadLine();
+            } while (true);
+        }
+        //Check
+        private static void Introduce()
+        {
+            Console.WriteLine("This is prime number detecor. I will check if given number is a number, " +
+                              "a prime number, count all factorials and print all prime factorials. Have fun!");
+            Console.Write("Type Your number: ");
+        }
+
+        private static void Check()
+        {
+
+        }
+
+        private static void InputNotNumber()
+        {
+            Console.WriteLine("This is not a number. Press ENTER to continue...");
+            Console.ReadLine();
+        }
+
+        private static void IsPrimeNumber(bool value)
+        {
+            Console.WriteLine(!value ? "Given number is not a prime number." : "Given number is a prime number.");
+        }
+
+        private static void CountFactors(List<ulong> input)
+        {
+            Console.WriteLine($"Given number have { input.Count } unique (not 1 and itself) factors.");
+        }
+
+        private static void ShowPrimeFactors(List<ulong> input)
+        {
+            var primeFactorials = PrimeNumberDetector.RetrievePrimeNumbers(input).OrderByDescending(z => z);
+
+            if (!primeFactorials.Any())
+            {
+                Console.WriteLine("Given number doesn't have any uniqe prime factors.");
+                return;
+            }
+
+            var finalMessage = $"All prime factors (from biggest one): ";
+            foreach (var number in primeFactorials)
+                finalMessage += $"{number}, ";
+
+            finalMessage = finalMessage.Remove(finalMessage.Length - 2);
+            finalMessage += ".";
+
+            Console.WriteLine(finalMessage);
+        }
+    }
+}
